0ba162050111937c707535813501d13f6567a9ef
git-armandojr <mail.armando@gmail.com>
2018-10-07 15:11:33 -0300

    format

--- src/br/edu/cefsa/ftt/bean/TipoBEAN.java
+++ src/br/edu/cefsa/ftt/bean/TipoBEAN.java
@@ -2,34 +2,33 @@ package br.edu.cefsa.ftt.bean;
 
 public class TipoBEAN {
 	
-	private int Codigo;
-    private String Descricao;
-	
-    public TipoBEAN() {
+	private int codigo;
+    private String descricao;    
+        
+	public TipoBEAN() {
 		super();
 		// TODO Auto-generated constructor stub
-	}
-
+	}	
+	
 	public TipoBEAN(int codigo, String descricao) {
 		super();
-		Codigo = codigo;
-		Descricao = descricao;
+		this.codigo = codigo;
+		this.descricao = descricao;
 	}
 
 	public int getCodigo() {
-		return Codigo;
+		return codigo;
 	}
-
 	public void setCodigo(int codigo) {
-		Codigo = codigo;
+		this.codigo = codigo;
 	}
-
 	public String getDescricao() {
-		return Descricao;
+		return descricao;
 	}
-
 	public void setDescricao(String descricao) {
-		Descricao = descricao;
+		this.descricao = descricao;
 	}
+	
+    
     
 }
--- src/br/edu/cefsa/ftt/util/DbUtil.java
+++ src/br/edu/cefsa/ftt/util/DbUtil.java
@@ -1,4 +1,4 @@
-package br.com.cefsa.ftt.util;
+package br.edu.cefsa.ftt.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
