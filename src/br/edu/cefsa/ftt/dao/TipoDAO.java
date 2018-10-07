c300e23d255ca7ab0e8df208b84ed9f23454c33c
SrMartis <sr.martins.ferr@gmail.com>
2018-10-07 18:06:37 -0300

    Método Insert para Tipo DAO

--- src/br/edu/cefsa/ftt/dao/TipoDAO.java
+++ src/br/edu/cefsa/ftt/dao/TipoDAO.java
@@ -1,5 +1,23 @@
 package br.edu.cefsa.ftt.dao;
 
-public class TipoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import br.edu.cefsa.ftt.bean.TipoBEAN;
+import br.edu.cefsa.ftt.util.DbUtil;
+
+public class TipoDAO {
+	public static void Inserir(TipoBEAN tipo) {
+	try{
+		String insert = "insert into tipos values (?,?)";
+		Connection connection = DbUtil.getConnection();
+		PreparedStatement sql = connection.prepareStatement(insert);
+		sql.executeUpdate();
+		
+	} catch (SQLException erro) {
+		
+	}
+		
+	}
 }
