f2230b52c075d260e7e029e8040710586d14cd20
git-armandojr <mail.armando@gmail.com>
2018-10-08 00:41:05 -0300

    Tipo funcionando

--- src/br/edu/cefsa/ftt/dao/TipoDAO.java
+++ src/br/edu/cefsa/ftt/dao/TipoDAO.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.edu.cefsa.ftt.bean.TipoBEAN;
 import br.edu.cefsa.ftt.util.DbUtil;
@@ -16,6 +19,105 @@ import br.edu.cefsa.ftt.util.MyException;
 	public TipoDAO() {
 		connection = DbUtil.getConnection();
 	}
+	
+	public void addTipo(TipoBEAN tipo) {
+        
+    	try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("INSERT INTO tipo (descricao) VALUES (?)");
+            
+            // Parameters start with 1
+            preparedStatement.setString(1, tipo.getDescricao());            
+            
+            preparedStatement.executeUpdate();            
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            
+            throw new ArithmeticException("TipoDAO: addTipo: " + e.getMessage()); 
+        }
+    } //addTipo
+	
+	public void deleteTipo(TipoBEAN tipo) throws MyException {
+        try {
+            
+        	PreparedStatement preparedStatement = connection
+                    .prepareStatement("DELETE FROM tipo WHERE codigo=?");
+            
+            // Parameters start with 1
+            preparedStatement.setInt(1, tipo.getCodigo());
+
+            int updates = preparedStatement.executeUpdate();
+            
+            if(updates == 0) {
+            	String message = "Código do tipo " + tipo.getCodigo() + " não encontrado!";
+            	
+            	throw new MyException(message);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } //deleteTipo
+	
+	public void updateTipo(TipoBEAN tipo) throws MyException {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("UPDATE tipo SET descricao=?" 
+                                               + "WHERE codigo=?");
+            
+            // Parameters start with 1
+            preparedStatement.setString(1, tipo.getDescricao());
+            preparedStatement.setInt(2, tipo.getCodigo());
+            
+            int updates = preparedStatement.executeUpdate();
+            
+            if(updates == 0) {
+            	String message = "Código do tipo " + tipo.getCodigo() + " não encontrado!";
+            	
+            	throw new MyException(message);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } //updateTipo
+	
+	public List<TipoBEAN> getAllTipos() throws MyException {
+        
+    	List<TipoBEAN> tipos = new ArrayList<TipoBEAN>();
+        
+        try {
+            Statement statement = connection.createStatement();
+            
+            ResultSet rs = statement.executeQuery("SELECT * FROM tipo");
+            
+            boolean found = false;
+            
+            while (rs.next()) {
+            	
+            	found = true;
+                
+            	TipoBEAN tipo = new TipoBEAN();
+                
+            	tipo.setCodigo(rs.getInt("codigo"));
+            	tipo.setDescricao(rs.getString("descricao"));
+
+                tipos.add(tipo);
+            }
+            
+            if (!found) {
+            	String message = "Nenhum tipo disponível!";
+            	
+            	throw new MyException(message);
+			}
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return tipos;
+    } //getAllTipos
 	
 	public TipoBEAN getTipoById(int id) throws MyException {
 		
@@ -43,5 +145,4 @@ import br.edu.cefsa.ftt.util.MyException;
 
         return tipo;
 	}
-
 }
